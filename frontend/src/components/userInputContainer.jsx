1788cc7070822472e55a72f5816704b18bb460f2
Mike MacDougall <mgmacdougall@outlook.com>
2025-11-03 12:49:49 -0500

    Add of tailwind class to user input component.

--- frontend/src/components/userInputContainer.jsx
+++ frontend/src/components/userInputContainer.jsx
@@ -16,39 +16,98 @@ function UserInputContainer({ onSubmit }) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="user-input">
-          User Name:
-          <input
-            id="user-input"
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder="Enter something..."
-          />
-        </label>
-        <label htmlFor="user-name-input">
-          Password:
-          <input
-            id="user-name-input"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter something..."
-          />
-        </label>
-        <label htmlFor="user-email-input">
-          Email:
-          <input
-            id="user-email-input"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter something..."
-          />
-        </label>
-        <button className="" type="submit">
-          Submit
-        </button>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base/7 font-semibold text-gray-900">
+              User Profile Creation Form
+            </h2>
+            <p className="mt-1 text-sm/6 text-gray-600">
+              Create you user Account.
+            </p>
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label
+                  className="block text-sm/6 font-medium text-gray-900"
+                  htmlFor="user-input"
+                >
+                  User Name:
+                </label>
+                <div className="mt-2">
+                  <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                    <div>
+                      <input
+                        id="user-input"
+                        type="text"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        placeholder="Enter something..."
+                        className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="user-name-input"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Password:
+                </label>
+                <div className="mt-2">
+                  <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                    <div>
+                      <input
+                        id="user-name-input"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter something..."
+                        className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="user-email-input">Email:</label>
+                <div className="mt-2">
+                  <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                    <div>
+                      <input
+                        id="user-email-input"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter something..."
+                        className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+              <button
+                type="button"
+                class="text-sm/6 font-semibold text-gray-900"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
