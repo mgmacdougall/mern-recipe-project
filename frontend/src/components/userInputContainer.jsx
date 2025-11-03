f93be35dfab5f044bf0d7326213bb16c87ea47fc
Mike MacDougall <mgmacdougall@outlook.com>
2025-11-03 18:13:06 -0500

    Update css

--- frontend/src/components/userInputContainer.jsx
+++ frontend/src/components/userInputContainer.jsx
@@ -92,16 +92,16 @@ function UserInputContainer({ onSubmit }) {
               </div>
             </div>
 
-            <div class="mt-6 flex items-center justify-end gap-x-6">
+            <div className="mt-6 flex items-center justify-end gap-x-6">
               <button
                 type="button"
-                class="text-sm/6 font-semibold text-gray-900"
+                className="text-sm/6 font-semibold text-gray-900"
               >
                 Cancel
               </button>
               <button
                 type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Submit
               </button>
