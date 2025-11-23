85c711c35eae965d7b6673d7a98ace0741fbccf5
Mike MacDougall <mgmacdougall@outlook.com>
2025-11-23 09:32:55 -0500

    Update

--- frontend/src/components/RecipeBoxList.jsx
+++ frontend/src/components/RecipeBoxList.jsx
@@ -1,33 +1,13 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import RecipeBoxCard from "./recipeBoxCard.jsx";
 
-function RecipeBoxList() {
-  const [recipeBoxes, setRecipeBoxes] = useState([]);
-
-  useEffect(() => {
-    async function fetchRecipeBoxes() {
-      try {
-        const response = await fetch("http://localhost:8080/recipe-box/all");
-        const data = await response.json();
-        setRecipeBoxes(data);
-      } catch (error) {
-        console.error("Error fetching recipe boxes:", error);
-      }
-    }
-
-    fetchRecipeBoxes();
-  }, []);
-
+function RecipeBoxList({ data }) {
   return (
     <>
       <h2 className="text-2xl font-bold mb-4">Recipe Boxes</h2>
-      {recipeBoxes.map(
-        (box) => (
-          // <div key={box.id ?? index}>{box.name ?? box.title ?? "Recipe Box"}</div>
-          console.log(Array.isArray(recipeBoxes)),
-          (<RecipeBoxCard key={box._id} data={box} />)
-        )
-      )}
+      {data.map((box) => (
+        <RecipeBoxCard key={box._id} data={box} />
+      ))}
     </>
   );
 }
